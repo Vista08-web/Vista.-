d5c3d24a105c06292d80e2e0f17ee45ac878f3d0
Vista08-web <rokoth09@gmail.com>
2025-10-22 09:53:22 +0300

    pages

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ document.querySelector("button").addEventListener("click", function() {
     alert("Button clicked!");
 });
 radioButtons.forEach(function(button) {
+    button.addEventListener("change", function() {
+        alert("Radio button changed!");
+    });
+});
